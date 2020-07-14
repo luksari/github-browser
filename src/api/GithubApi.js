0e9f3744f44c9c16989ae3b3ff3f74d56bd05e51
luksari <luksari23@gmail.com>
2020-07-14 19:18:26 +0200

    Render events list

--- src/api/GithubApi.js
+++ src/api/GithubApi.js
@@ -3,4 +3,7 @@ import { getRequest } from "../utils/requests";
 export class GithubApi {
   static getUserProfile = (userName) => 
     getRequest(`https://api.github.com/users/${userName}`)
+    
+  static getUserEvents = (userName) => 
+    getRequest(`https://api.github.com/users/${userName}/events/public`)
 }
